9a2a491779fc9f6ab24f6b371f30023b058ff4f3
MicheleSr <mikefender@cryptolab.net>
2015-02-09 19:17:49 +0100

    add fft and ifft

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
-CFLAGS=-ljack -lpthread -ldl -lrt -lm -O2
-DFLAGS=-ljack -lpthread -ldl -lrt -lm -g
+CFLAGS=-ljack -lpthread -ldl -lrt -lm -lfftw3 -O2
+DFLAGS=-ljack -lpthread -ldl -lrt -lm -lfftw3 -g
 PREFIX=bin
 OBJ=$(PREFIX)/obj
 SRC=src
@@ -8,6 +8,10 @@ SRC=src
 $(PREFIX)/filter: $(SRC)/filter.c Makefile
 	mkdir -p $(PREFIX)
 	gcc $(SRC)/filter.c -o $(PREFIX)/filter $(CFLAGS)
+
+debug:
+	mkdir -p $(PREFIX)
+	gcc $(SRC)/filter.c -o $(PREFIX)/filter_db $(DFLAGS)
 
 clean:
 	rm -fr $(OBJ)
--- src/filter.c
+++ src/filter.c
@@ -21,6 +21,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <fftw3.h>
 
 /* jack libs */
 #include <jack/jack.h>
@@ -29,32 +30,77 @@
 typedef jack_default_audio_sample_t sample_t;
 
 jack_port_t *in_p, *out_p;
-jack_nframes_t sr;
+jack_nframes_t sr, bs;
+fftw_complex *dft;
+fftw_plan dft_p, idft_p;
+double *dft_in, *idft;
+sample_t *in;
 
 /* function declaration */
 
 int process(jack_nframes_t, void *);
 int srate(jack_nframes_t, void *);
+int bsize(jack_nframes_t, void *);
+void data_init();
 void jack_shutdown(void *); 
 
 /* function definition */
 
+/* initialize fft plans and arrays */
+void data_init() {
+  if (idft != NULL) {
+    free(idft);
+    fftw_destroy_plan(idft_p);
+  }
+
+  if (dft != NULL) {
+    fftw_destroy_plan(dft_p);
+    fftw_free(dft);
+  }
+
+  dft_in = calloc(sizeof(double), bs);
+  dft = fftw_malloc(sizeof(fftw_complex) * bs);
+  idft = calloc(sizeof(double), bs);
+  dft_p = fftw_plan_dft_r2c_1d(bs, dft_in, dft, FFTW_ESTIMATE);
+  idft_p = fftw_plan_dft_c2r_1d(bs, dft, idft, FFTW_ESTIMATE);
+}
+
+/* called by jack sr/bs times per seconds */
 int process(jack_nframes_t nframes, void *arg) {
-  sample_t *in = (sample_t *) jack_port_get_buffer (in_p, nframes);
-  sample_t *out = (sample_t *) jack_port_get_buffer (out_p, nframes);
+  in = (sample_t *) jack_port_get_buffer (in_p, nframes);
   int i;
   for(i=0; i < nframes; i++) {
-    out[i] = in[i];
+    dft_in[i] = in[i];
+  }
+  /* calculating dft of the signal */
+  fftw_execute(dft_p);
+
+  /* calculating idft of signal spectrum */
+  fftw_execute(idft_p);
+  sample_t *out = (sample_t *) jack_port_get_buffer (out_p, nframes);
+  for(i=0; i < nframes; i++) {
+    /* we need to normalize the signal */
+    out[i] = idft[i]/bs;
   }
   return 0;  
 }
 
+/* called at start and on sr change */
 int srate(jack_nframes_t nframes, void *arg) {
-  printf("Sample Rate = %" PRIu32 "/sec\n", nframes);
+  printf("Sample Rate = %d/sec\n", nframes);
   sr = nframes;
   return 0;
 }
 
+/* called at start and on bs change */
+int bsize(jack_nframes_t nframes, void *arg) {
+  printf("Buffer size = %d\n", nframes);
+  bs = nframes;
+  data_init();
+  return 0;
+}
+
+/* called on jack shutdown */
 void jack_shutdown(void *arg) {
   exit(1);
 }
@@ -77,6 +123,7 @@ int main(int argc, char **argv) {
 
   jack_set_process_callback(client, process, 0);
   jack_set_sample_rate_callback(client, srate, 0);
+  jack_set_buffer_size_callback(client, bsize, 0);
   jack_on_shutdown(client, jack_shutdown, 0);
 
   in_p = jack_port_register(client, "in", JACK_DEFAULT_AUDIO_TYPE, JackPortIsInput, 0);
